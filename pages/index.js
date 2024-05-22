b517829102ff464744448ced4d464aad524904de
Joaoaalves <joaoaa21@hotmail.com>
2024-05-22 19:32:27 -0300

    Added next themes for dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
 const font = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
   return (
     <main
       className={`${font.className}`}
